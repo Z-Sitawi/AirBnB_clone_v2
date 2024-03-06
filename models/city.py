16eee14c8964791becc42a638df6a478652a6d6b
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-07 00:02:23 +0100

    fix imports

--- models/city.py
+++ models/city.py
@@ -2,7 +2,6 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -17,4 +16,4 @@ class City(BaseModel, Base):
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(ForeignKey('states.id'), String(60), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
@@ -28,7 +27,7 @@ class State(BaseModel, Base):
             """Get a list of all related City objects."""
             from models import storage
             city_list = []
-            for city in list(storage.all(City).values()):
+            for city in list(storage.all("City").values()):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
