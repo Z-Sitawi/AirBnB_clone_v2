e9eca3427189f35e0a52a9357ba2df23f248c6ae
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-09 12:45:55 +0100

    update user model

--- models/user.py
+++ models/user.py
@@ -25,3 +25,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    place = relationship('Place', backref='user', cascade="delete")
