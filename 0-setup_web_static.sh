502b88294e286debac324e8ee12378136bf997f2
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-10 17:23:19 +0000

    Prepare my web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,47 +1,17 @@
 #!/usr/bin/env bash
-# Sets up your web servers for the deployment of web_static
+# Sets up a web server for deployment of web_static.
 
-# Updates the package list & Installs Nginx if it not already installed
-sudo apt-get update
+apt-get update
 apt-get install -y nginx
 
-# Create the folder /data/ if it does not already exist
-sudo mkdir -p "/data/"
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create the folder /data/web_static/ if it does not already exist
-sudo mkdir -p "/data/web_static/"
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create the folder /data/web_static/releases/ if it does not already exist
-sudo mkdir -p "/data/web_static/releases/"
-
-# Create the folder /data/web_static/shared/ if it does not already exist
-sudo mkdir -p "/data/web_static/shared/"
-
-# Create the folder /data/web_static/releases/test/ if it not exist
-sudo mkdir -p "/data/web_static/releases/test/"
-
-# Create a fake HTML file /data/web_static/releases/test/index.html
-# (with simple content, to test your Nginx configuration)
-sudo touch "/data/web_static/releases/test/index.html"
-echo "Hello Morocco!" > /data/web_static/releases/test/index.html
-
-# Create a symbolic link /data/web_static/current
-# linked to the /data/web_static/releases/test/ folder.
-# If the symbolic link already exists, it should be deleted
-# and recreated every time the script is ran.
-if [ -L "/data/web_static/current" ]; then
-    # If it exists, delete it
-    sudo rm "/data/web_static/current"
-fi
-ln -s "/data/web_static/releases/test/" /data/web_static/current
-
-# Give ownership of the /data/ folder to the ubuntu user AND group.
-# This should be recursive;
-# everything inside should be created/owned by this user/group.
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update the Nginx configuration
-# to serve the content of /data/web_static/current/ to hbnb_static
 printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
@@ -49,7 +19,7 @@ printf %s "server {
     root   /var/www/html;
     index  index.html index.htm;
 
-    location https://www.z-sitawi.tech/hbnb_static {
+    location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
@@ -65,5 +35,4 @@ printf %s "server {
     }
 }" > /etc/nginx/sites-available/default
 
-# Restart Nginx to apply the changes
-sudo service nginx restart
+service nginx restart
