04231de226b3d77dce5e5740b351db55cf35c04d
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-25 00:34:27 +0000

    hbnb routr

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask
+
+# Create a Flask web application
+app = Flask(__name__)
+
+# Define routes
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+# Run the Flask app
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
