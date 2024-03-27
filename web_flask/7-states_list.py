ecbe9e41e05aaafb02ca782179b8562b2ade270d
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-03-27 22:41:37 +0000

    states list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,26 +2,23 @@
 """
 Starts a Flask web application
 """
-
 from flask import Flask, render_template
 from models import *
 from models import storage
-
 app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display an HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the current SQLAlchemy Session"""
     storage.close()
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Displays a HTML page with a list of all State objects"""
-    states = storage.all("State").values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
 
 
 if __name__ == "__main__":
